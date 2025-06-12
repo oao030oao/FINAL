9e687262547b25f2e9c073959ca9d76f43f899c9
oao030oao <169072682+oao030oao@users.noreply.github.com>
2025-06-12 22:17:27 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import google.generativeai as genai
 import os
 
+
+
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-# 使用 Render 環境變數（建議這樣做）
-
+# ========== Gemini API 初始化 ==========
 genai.configure(api_key=GEMINI_API_KEY)
-
+model = genai.GenerativeModel("gemini-2.0-flash")
 app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "吵架不能沒有記憶")
 
@@ -16,7 +17,6 @@ role_B_prompt = "你是一位冷靜又強詞奪理的人，請針對主題反駁
 def generate_reply(prompt_prefix, history):
     try:
         prompt = prompt_prefix + "\n" + "\n".join(history)
-        model = genai.GenerativeModel("gemini-2.0-flash")
         response = model.generate_content(
             prompt,
             generation_config=genai.types.GenerationConfig(
